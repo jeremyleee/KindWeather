24c1011e6f1e69a992fafe89661b97b23d324475
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-02-26 22:09:11 +1300

    Setup Realm

---
+++ app/src/main/java/com/tragicfruit/weatherapp/WApplication.kt
@@ -0,0 +1,20 @@
+package com.tragicfruit.weatherapp
+
+import android.app.Application
+import io.realm.Realm
+import io.realm.RealmConfiguration
+
+class WApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Realm.init(this)
+        Realm.setDefaultConfiguration(RealmConfiguration.Builder()
+                .name("WeatherAppDB")
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded() // TODO: remove on production
+                .build())
+    }
+
+}
