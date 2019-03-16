67fb9351176827b480944d41cb2df1a6b637f847
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-03-17 00:57:34 +1300

    Add test alerts for debugging

--- app/src/main/java/com/tragicfruit/weatherapp/WApplication.kt
+++ app/src/main/java/com/tragicfruit/weatherapp/WApplication.kt
@@ -3,10 +3,13 @@ package com.tragicfruit.weatherapp
 import android.app.Application
 import com.tragicfruit.weatherapp.controllers.NotificationController
 import com.tragicfruit.weatherapp.controllers.WeatherController
+import com.tragicfruit.weatherapp.model.ForecastType
+import com.tragicfruit.weatherapp.model.WeatherAlert
 import com.tragicfruit.weatherapp.utils.SharedPrefsHelper
 import com.tragicfruit.weatherapp.utils.ViewHelper
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import io.realm.kotlin.where
 import timber.log.Timber
 
 class WApplication : Application() {
@@ -30,6 +33,26 @@ class WApplication : Application() {
 
         WeatherController.init()
         NotificationController.init(this)
+
+        // Adding some test alerts
+        // TODO: shift this to a controller
+        val realm = Realm.getDefaultInstance()
+        if (realm.where<WeatherAlert>().count() == 0L) {
+            realm.executeTransaction {
+                val umbrellaAlert = WeatherAlert.create("Umbrella", "Remember to bring your umbrella!", 1, it)
+                WeatherAlert.addParam(umbrellaAlert, ForecastType.Rain_probability, 0.5, null, it)
+                WeatherAlert.addParam(umbrellaAlert, ForecastType.Rain_intensity, 10.0, null, it)
+                WeatherAlert.addParam(umbrellaAlert, ForecastType.Wind_gust, null, 40.0, it)
+
+                val rainJacketAlert = WeatherAlert.create("Rain jacket", "Bring your rain jacket today!", 2, it)
+                WeatherAlert.addParam(rainJacketAlert, ForecastType.Rain_probability, 0.5, null, it)
+                WeatherAlert.addParam(rainJacketAlert, ForecastType.Rain_intensity, 10.0, null, it)
+                WeatherAlert.addParam(rainJacketAlert, ForecastType.Wind_gust, 40.0, null, it)
+
+                val sunscreenAlert = WeatherAlert.create("Sunscreen", "Put on some sunscreen before you go out", 3, it)
+                WeatherAlert.addParam(sunscreenAlert, ForecastType.Uv_index, 3.0, null, it)
+            }
+        }
 
     }
 
