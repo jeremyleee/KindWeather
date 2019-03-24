2f7541d8f934120f7a959766d6d56206800b069b
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-03-25 11:53:59 +1300

    Update UI design for alert cell

--- app/src/main/java/com/tragicfruit/kindweather/components/AlertCell.kt
+++ app/src/main/java/com/tragicfruit/kindweather/components/AlertCell.kt
@@ -3,8 +3,9 @@ package com.tragicfruit.kindweather.components
 import android.content.Context
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.view.Gravity
 import android.widget.ImageView
-import android.widget.RelativeLayout
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
@@ -14,33 +15,33 @@ import com.tragicfruit.kindweather.model.WeatherAlert
 import com.tragicfruit.kindweather.utils.ViewHelper
 import com.tragicfruit.kindweather.utils.setMargins
 
-class AlertCell(context: Context, private val listener: Listener? = null) : RelativeLayout(context) {
+class AlertCell(context: Context, private val listener: Listener? = null) : LinearLayout(context) {
 
-    private val cellContainer = RelativeLayout(context)
-    private val backgroundView = ImageView(context)
     private val nameView = TextView(context)
     private val illustrationView = AlertCellImageView(context)
 
     init {
-        addView(cellContainer, LayoutParams(LayoutParams.MATCH_PARENT, ViewHelper.parsePx(R.dimen.alert_cell_height)).apply {
+        layoutParams = LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT).apply {
             setMargins(ViewHelper.parsePx(R.dimen.app_margin_xx), ViewHelper.parsePx(R.dimen.app_margin))
-        })
+        }
 
-        backgroundView.setImageResource(R.drawable.alert_cell_background)
-        cellContainer.addView(backgroundView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT)
-
-        illustrationView.adjustViewBounds = true
-        illustrationView.scaleType = ImageView.ScaleType.FIT_XY
-        cellContainer.addView(illustrationView, LayoutParams(ViewHelper.toPx(220), LayoutParams.WRAP_CONTENT).apply {
-            addRule(ALIGN_PARENT_END)
-            setMargins(0, ViewHelper.parsePx(R.dimen.app_margin), ViewHelper.parsePx(R.dimen.app_margin_xx), 0)
-        })
+        orientation = HORIZONTAL
+        setBackgroundResource(R.drawable.alert_cell_background)
 
         nameView.textSize = 36f
         nameView.typeface = ResourcesCompat.getFont(context, R.font.playfair_bold)
         nameView.setTextColor(ContextCompat.getColor(context, R.color.alert_title))
-        cellContainer.addView(nameView, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
-            setMargins(ViewHelper.parsePx(R.dimen.app_margin_xxx), ViewHelper.toPx(42), 0, 0)
+        nameView.gravity = Gravity.CENTER_VERTICAL
+        addView(nameView, LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.MATCH_PARENT).apply {
+            setMargins(ViewHelper.parsePx(R.dimen.app_margin_xxx), 0, 0, 0)
+            weight = 1f
+        })
+
+        illustrationView.adjustViewBounds = true
+        illustrationView.scaleType = ImageView.ScaleType.FIT_XY
+        addView(illustrationView, LinearLayout.LayoutParams(0, LayoutParams.WRAP_CONTENT).apply {
+            setMargins(0, ViewHelper.parsePx(R.dimen.app_margin), ViewHelper.parsePx(R.dimen.app_margin_xx), 0)
+            weight = 1f
         })
     }
 
@@ -53,12 +54,17 @@ class AlertCell(context: Context, private val listener: Listener? = null) : Rela
 
         val backgroundColor = if (alert.enabled) {
             ContextCompat.getColor(context, alert.getInfo().color)
-        } else Color.GRAY
-        backgroundView.setColorFilter(backgroundColor, PorterDuff.Mode.SRC_IN)
+        } else Color.LTGRAY
+        background.setColorFilter(backgroundColor, PorterDuff.Mode.SRC_IN)
 
         Glide.with(this)
             .load(alert.getInfo().image)
             .into(illustrationView)
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        val height = MeasureSpec.getSize(widthMeasureSpec) * 0.5625
+        super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(height.toInt(), MeasureSpec.EXACTLY))
     }
 
     interface Listener {
