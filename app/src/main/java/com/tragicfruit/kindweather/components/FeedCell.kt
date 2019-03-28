f880da9a686cb339bf6827f1cb1889346bd8d29c
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-03-29 12:44:49 +1300

    Add shadows to feed cells

--- app/src/main/java/com/tragicfruit/kindweather/components/FeedCell.kt
+++ app/src/main/java/com/tragicfruit/kindweather/components/FeedCell.kt
@@ -9,12 +9,14 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.setMargins
 import androidx.core.view.setPadding
 import com.tragicfruit.kindweather.R
 import com.tragicfruit.kindweather.controllers.ForecastIcon
 import com.tragicfruit.kindweather.model.ForecastType
 import com.tragicfruit.kindweather.model.WeatherNotification
 import com.tragicfruit.kindweather.utils.DisplayUtils
+import com.tragicfruit.kindweather.utils.RoundRectOutlineProvider
 import com.tragicfruit.kindweather.utils.ViewHelper
 import com.tragicfruit.kindweather.utils.getViewId
 
@@ -27,17 +29,24 @@ class FeedCell(context: Context, private val listener: Listener? = null) : Linea
     private val descriptionView = TextView(context)
 
     init {
-        setPadding(ViewHelper.parsePx(R.dimen.app_margin_xx))
         orientation = VERTICAL
 
         dateView.setTextColor(ContextCompat.getColor(context, R.color.text_lt_grey))
         addView(dateView, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
-            setMargins(0, 0, 0, ViewHelper.parsePx(R.dimen.app_margin_xx))
+            setMargins(
+                ViewHelper.parsePx(R.dimen.app_margin_xx),
+                ViewHelper.parsePx(R.dimen.app_margin_xx),
+                ViewHelper.parsePx(R.dimen.app_margin_xx),
+                0)
         })
 
-        cellContainer.setBackgroundResource(R.drawable.alert_cell_background)
+        cellContainer.setBackgroundResource(R.drawable.round_rect_cell_background)
+        cellContainer.elevation = ViewHelper.parsePx(R.dimen.app_margin).toFloat()
+        cellContainer.outlineProvider = RoundRectOutlineProvider(ViewHelper.parsePx(R.dimen.round_rect_cell_radius).toFloat())
         cellContainer.setPadding(ViewHelper.parsePx(R.dimen.app_margin_xx))
-        addView(cellContainer, LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT)
+        addView(cellContainer, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
+            setMargins(ViewHelper.parsePx(R.dimen.app_margin_xx))
+        })
 
         icon.scaleType = ImageView.ScaleType.FIT_CENTER
         cellContainer.addView(icon, ViewHelper.parsePx(R.dimen.weather_icon_size), ViewHelper.parsePx(R.dimen.weather_icon_size))
