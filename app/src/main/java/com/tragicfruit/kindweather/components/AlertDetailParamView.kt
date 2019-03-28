c50a8fcd59e2b22a965e9ffbeaaf77a01b6cccfa
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-03-29 11:49:47 +1300

    Increase range seekbar thumb size

--- app/src/main/java/com/tragicfruit/kindweather/components/AlertDetailParamView.kt
+++ app/src/main/java/com/tragicfruit/kindweather/components/AlertDetailParamView.kt
@@ -4,14 +4,11 @@ import android.content.Context
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.core.content.ContextCompat
-import androidx.core.view.setPadding
 import com.jaygoo.widget.OnRangeChangedListener
 import com.jaygoo.widget.RangeSeekBar
 import com.tragicfruit.kindweather.R
 import com.tragicfruit.kindweather.model.WeatherAlertParam
-import com.tragicfruit.kindweather.utils.DisplayUtils
-import com.tragicfruit.kindweather.utils.ViewHelper
-import com.tragicfruit.kindweather.utils.getViewId
+import com.tragicfruit.kindweather.utils.*
 
 class AlertDetailParamView(context: Context) : RelativeLayout(context) {
 
@@ -24,11 +21,17 @@ class AlertDetailParamView(context: Context) : RelativeLayout(context) {
     var upperBound: Double? = null; private set
 
     init {
-        setPadding(ViewHelper.parsePx(R.dimen.app_margin_xx))
+        setPadding(0, ViewHelper.parsePx(R.dimen.app_margin_xx))
 
         paramTitle.setTextColor(ContextCompat.getColor(context, R.color.text_black))
-        addView(paramTitle, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT))
+        addView(paramTitle, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
+            setMargins(ViewHelper.parsePx(R.dimen.app_margin_xx), 0)
+        })
 
+        slider.leftSeekBar.thumbSize = ViewHelper.parsePx(R.dimen.seekbar_thumb_size)
+        slider.rightSeekBar.thumbSize = ViewHelper.parsePx(R.dimen.seekbar_thumb_size)
+        slider.leftSeekBar.thumbDrawableId = R.drawable.seekbar_thumb
+        slider.rightSeekBar.thumbDrawableId = R.drawable.seekbar_thumb
         addView(slider, LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT).apply {
             addRule(BELOW, paramTitle.getViewId())
             setMargins(0, ViewHelper.parsePx(R.dimen.app_margin), 0, 0)
@@ -38,14 +41,14 @@ class AlertDetailParamView(context: Context) : RelativeLayout(context) {
         addView(lowerBoundText, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
             addRule(BELOW, slider.getViewId())
             addRule(ALIGN_PARENT_LEFT)
-            marginStart = ViewHelper.parsePx(R.dimen.app_margin)
+            marginStart = ViewHelper.parsePx(R.dimen.app_margin_xxx)
         })
 
         upperBoundText.setTextColor(ContextCompat.getColor(context, R.color.text_grey))
         addView(upperBoundText, LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT).apply {
             addRule(BELOW, slider.getViewId())
             addRule(ALIGN_PARENT_RIGHT)
-            marginEnd = ViewHelper.parsePx(R.dimen.app_margin)
+            marginEnd = ViewHelper.parsePx(R.dimen.app_margin_xxx)
         })
     }
 
