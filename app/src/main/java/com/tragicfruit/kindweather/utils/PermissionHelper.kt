d3b001f91e6f0a310cf42ba03d0bce7f59e3f8e8
Jeremy Lee <jeremy@jeremylee.co.nz>
2019-08-27 14:05:50 +0200

    Fix location permissions for API 28 and older

--- app/src/main/java/com/tragicfruit/kindweather/utils/PermissionHelper.kt
+++ app/src/main/java/com/tragicfruit/kindweather/utils/PermissionHelper.kt
@@ -3,11 +3,16 @@ package com.tragicfruit.kindweather.utils
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.core.content.ContextCompat
 
 object PermissionHelper {
 
-    val FULL_LOCATION = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    val FULL_LOCATION = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION)
+    } else {
+        arrayOf(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
 
     fun hasPermission(context: Context, permission: String) =
         ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
